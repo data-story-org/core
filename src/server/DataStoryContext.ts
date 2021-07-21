7fe1e68727faaed1fab66da5ce3c408f78035b47
Anders Jurisoo <jurisoo@hotmail.com>
2021-07-21 16:23:22 +0200

    Add string indexer to allow for extra keys

--- src/server/DataStoryContext.ts
+++ src/server/DataStoryContext.ts
@@ -3,4 +3,6 @@ export interface DataStoryContext {
 	apis?: {},
 	// Used to expose data as named ResolveContextFeature Nodes
 	models?: {},
+	// We may put custom keys in the context
+	[others: string]: any;
 }
