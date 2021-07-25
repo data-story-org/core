b6f1f8f0e1fe64064d2561a10381dc26e1f90129
Anders Jurisoo <jurisoo@hotmail.com>
2021-07-25 18:29:10 +0200

    Add CLI test stub

--- tests/Unit/cli/cli.test.ts
+++ tests/Unit/cli/cli.test.ts
@@ -1,4 +1,8 @@
 import { spawn } from 'child_process';
+import { DiagramBuilder } from '../../../src/server/DiagramBuilder';
+import CreateJSON from '../../../src/server/nodes/CreateJSON'
+import Inspect from '../../../src/server/nodes/Inspect'
+import { nonCircularJsonStringify } from '../../../src/utils/nonCircularJsonStringify';
 
 /** SPAWN A CHILD PROCESS AND CAPTURE STDOUT AND STDERR */
 async function spawnChild(command, arguments_, options) {
@@ -38,6 +42,17 @@ async function cli($command, ...$args) {
 	)
 }
 
+function addslashes(string) {
+	return string.replace(/\\/g, '\\\\').
+			replace(/\u0008/g, '\\b').
+			replace(/\t/g, '\\t').
+			replace(/\n/g, '\\n').
+			replace(/\f/g, '\\f').
+			replace(/\r/g, '\\r').
+			replace(/'/g, '\\\'').
+			replace(/"/g, '\\"');
+}
+
 it('can boot via cli', async () => {
 	const success = (data) => {
 		const diagram = JSON.parse(data)
@@ -52,4 +67,26 @@ it('can boot via cli', async () => {
 	}
 
   await cli('boot').then(success, fail)
+});
+
+it.skip('can run via cli', async () => {
+	let diagram = DiagramBuilder.begin()
+		.add(CreateJSON)
+		.add(Inspect)
+		.finish()
+
+	const success = (data) => {
+		// const diagram: any = JSON.parse(data)
+		// expect(diagram.nodes[1].features .......).toBe(true)
+	}
+
+	const fail = (err) => {
+		console.error("async error:\n" + err)
+		throw 'Failed to run the CLI'
+	}
+
+	let json = nonCircularJsonStringify(diagram)
+	json = addslashes(json)
+
+  await cli('run "' + json + '"').then(success, fail)
 });
