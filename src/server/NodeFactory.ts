dc2f0a6b24ad060b15cb353d3a489a67a1327d6e
Lenivaya <Lenivaya@users.noreply.github.com>
2021-07-26 15:25:58 +0000

    Apply formatting changes

--- src/server/NodeFactory.ts
+++ src/server/NodeFactory.ts
@@ -27,7 +27,7 @@ import Sample from './nodes/Sample';
 import Sort from './nodes/Sort';
 import Sleep from './nodes/Sleep';
 import ThrowError from './nodes/ThrowError';
-import RemoveAttributes from './nodes/RemoveAttributes'
+import RemoveAttributes from './nodes/RemoveAttributes';
 
 import { SerializedNodeModel } from '../types/SerializedNodeModel';
 import { ApiNodeFactory } from './nodes/factories/ApiNodeFactory';
