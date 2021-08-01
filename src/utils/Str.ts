fbc4e028c18a0b275d63a0ec71636d3096617c34
Anders Jurisoo <jurisoo@hotmail.com>
2021-08-01 12:52:15 +0200

    No need for addSlashes since no CLI :)

--- src/utils/Str.ts
+++ src/utils/Str.ts
@@ -7,14 +7,3 @@ export const trim = (s: string, c: string): string => {
     '',
   );
 };
-
-export const addSlashes = (string: string): string => {
-	return string.replace(/\\/g, '\\\\').
-			replace(/\u0008/g, '\\b').
-			replace(/\t/g, '\\t').
-			replace(/\n/g, '\\n').
-			replace(/\f/g, '\\f').
-			replace(/\r/g, '\\r').
-			replace(/'/g, '\\\'').
-			replace(/"/g, '\\"');
-}
