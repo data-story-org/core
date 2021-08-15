edd49d455970dc10c465a94a2556f7ae9addf118
Lenivaya <xocada@gmail.com>
2021-08-15 20:26:06 +0300

    add support of primitive features

--- src/server/nodes/Filter.ts
+++ src/server/nodes/Filter.ts
@@ -16,16 +16,27 @@ export default class Filter extends Node {
   }
 
   async run() {
-    const toMatchAgainst =
-      this.getParameterValue('attribute').split('.');
+    const toMatchAgainst = this.getParameterValue(
+      'attribute to match against',
+    ).split('.');
     const ports = this.getParameterValue('Output ports');
 
     const isPortsConfigured = !(
       ports.length === 1 && ports[0] === 'port'
     );
 
+    const isAttributePrimitive =
+      toMatchAgainst.length === 1 &&
+      (toMatchAgainst[0] === '' ||
+        toMatchAgainst[0] === 'attribute to match' ||
+        toMatchAgainst[0] === 'none');
+
     const isMatchAgainst = (port) => (feature) => {
       const { original } = feature;
+
+      if (isAttributePrimitive) {
+        return original === port;
+      }
 
       if (toMatchAgainst.length > 1) {
         const data = toMatchAgainst.reduce((obj, path) => {
@@ -37,8 +48,8 @@ export default class Filter extends Node {
         return data === port;
       }
 
-      return toMatchAgainst in original
-        ? original[toMatchAgainst] === port
+      return toMatchAgainst[0] in original
+        ? original[toMatchAgainst[0]] === port
         : false;
     };
 
@@ -55,6 +66,10 @@ export default class Filter extends Node {
     const unmatched = this.input().filter((feature) => {
       const { original } = feature;
 
+      if (isAttributePrimitive) {
+        return !ports.includes(original);
+      }
+
       if (toMatchAgainst.length > 1) {
         const data = toMatchAgainst.reduce((obj, path) => {
           return obj && obj[path] === undefined
@@ -67,8 +82,8 @@ export default class Filter extends Node {
           : false);
       }
 
-      return !(toMatchAgainst in original
-        ? ports.includes(original[toMatchAgainst])
+      return !(toMatchAgainst[0] in original
+        ? ports.includes(original[toMatchAgainst[0]])
         : false);
     });
 
@@ -78,9 +93,11 @@ export default class Filter extends Node {
   getDefaultParameters() {
     return [
       ...super.getDefaultParameters(),
-      NodeParameter.string('attribute')
-        .withValue('attribute to match against')
-        .withDescription('you may use dot notated paths'),
+      NodeParameter.string('attribute to match against')
+        .withValue('attribute to match')
+        .withDescription(
+          'you may use dot notated paths, or none to match feature generally',
+        ),
       NodeParameter.port('Output ports', 'String_')
         .withValue('port')
         .repeatable(),
