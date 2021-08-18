d2613e39045ddb59588723833f18b3dd011104f2
Lenivaya <xocada@gmail.com>
2021-08-18 10:17:26 +0300

    delete modalStyle
    let the style be in the gui package

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,5 @@
-// export { get, pickBy } from './Obj';
-// export { groupBy } from './Arr';
-// export * as modalStyle from './modalStyle';
-// export * as UID from './UID';
-// export { nonCircularJsonStringify } from './nonCircularJsonStringify';
-// export { trim } from './Str';
-
 export * from './Obj';
 export * from './Arr';
-export * from './modalStyle';
 export * from './nonCircularJsonStringify';
 export * from './UID';
 export * from './Str';
