6e4ed77175e39bc11386c8f8f9f55e4239678751
Anders Jurisoo <jurisoo@hotmail.com>
2021-09-04 16:44:19 +0200

    Fix Multiply test

--- tests/Unit/server/nodes/Multiply.test.ts
+++ tests/Unit/server/nodes/Multiply.test.ts
@@ -1,8 +1,23 @@
 import { Multiply } from '../../../../src/server/nodes/Multiply'
 import { when } from "../NodeTester";
 
-it('will multiply numbers', async () => {	
-	await when(Multiply).hasInput([1,2,3])
-		.assertOutput([2,4,6])
-		.finish()
-});
+describe('DownloadJSON node', () => {
+	it('will multiply numbers with a default factor of 2', async () => {	
+		await when(Multiply).hasInput([1,2,3])
+			.assertOutput([2,4,6])
+			.finish()
+	});
+
+	it('will multiply numbers with configurable factor', async () => {	
+		await when(Multiply).hasInput([1,2,3])
+			.and().hasParameters({factor: 3})
+			.assertOutput([3,6,9])
+			.finish()
+	});	
+	
+	it('will yield NaN if recieving objects', async () => {	
+		await when(Multiply).hasInput([{}])
+			.assertOutput([NaN])
+			.finish()
+	});
+})
