3c62a4862cd13ad535bbf6c3b09be8acb1ffad80
Anders Jurisoo <jurisoo@hotmail.com>
2021-09-05 19:01:42 +0200

    Prepare for demos

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,6 @@ export {
 export { Feature } from './Feature';
 export { NodeParameter } from './NodeParameter';
 export { repeatableConverter } from './NodeParameter';
+
+export { demos } from './server/demos'
+export { DiagramBuilder } from './server/DiagramBuilder'
