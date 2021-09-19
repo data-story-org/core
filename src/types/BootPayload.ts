5efe37769569e4dc1f27d67dc7fa50cb972331ec
Lenivaya <xocada@gmail.com>
2021-09-19 14:13:30 +0300

    add serializedModel property to the BootPayload interface
    serializedModel is being used when booting on the gui side, so we need
    right typing's in our BootPayload interface

--- src/types/BootPayload.ts
+++ src/types/BootPayload.ts
@@ -1,6 +1,9 @@
+import { SerializedDiagram } from './SerializedDiagram';
+
 export interface BootPayload {
   data: {
     stories?: string[];
     availableNodes: object[];
+    serializedModel?: SerializedDiagram;
   };
 }
