cb8ad3775b4f6f887033ca2af2c16deb111ff9a8
Anders Jurisoo <jurisoo@hotmail.com>
2021-09-19 13:13:12 +0200

    Export Demo wrapper type

---
+++ src/server/demos/Demo.ts
@@ -0,0 +1,10 @@
+import { Diagram } from "../Diagram";
+
+export class Demo {
+	constructor(
+    public name: string,
+    public description: string,
+    public tags: string[],
+		public diagram: Diagram
+  ) {}
+}
---
+++ src/server/demos/index.ts
@@ -0,0 +1,31 @@
+import { DiagramBuilder } from '../DiagramBuilder';
+import { Demo } from './Demo'
+import {
+  Create,
+  Inspect,
+  HTTPRequest,
+} from '../nodes';
+
+const empty = new Demo(
+	'Empty',
+	'Start from scratch.',
+	['empty'],
+	DiagramBuilder.begin().finish()
+)
+
+const api = new Demo(
+	'Working with APIs',
+	'Call APIs and process output.',
+	['api', 'http'],
+	DiagramBuilder.begin()
+		.add(Create)
+		.add(HTTPRequest)
+		.add(Inspect)
+		.add(Inspect)
+		.add(Inspect)
+		.finish()	
+)
+
+export const demos = [empty, api];
+
+export { Demo } from './Demo';
