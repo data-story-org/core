7ceab4aee74f1a4cdadad17699da20d81761b89d
Lenivaya <xocada@gmail.com>
2021-09-20 20:18:04 +0300

    Add possibility to specify diagram type explicitly
    So we can explicitly change diagram type if we need it

--- src/server/DataStory.ts
+++ src/server/DataStory.ts
@@ -2,11 +2,11 @@ import { Diagram } from './Diagram';
 
 export type Tag = string;
 
-export class DataStory {
+export class DataStory<T = Diagram> {
   constructor(
     public name: string,
     public description: string,
     public tags: Tag[],
-    public diagram: Diagram,
+    public diagram: T,
   ) {}
 }
