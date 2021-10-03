5a5445295b7d65f639563bc086c03d3728a79ecb
ajthinking <ajthinking@users.noreply.github.com>
2021-10-03 14:44:25 +0000

    Apply formatting changes

--- src/server/nodes/ScrapeHTML.ts
+++ src/server/nodes/ScrapeHTML.ts
@@ -1,89 +1,109 @@
-import { Node } from "../Node";
-import { NodeParameter } from "../../NodeParameter";
-import axios, { AxiosRequestConfig } from "axios";
-import { Feature } from "../../Feature";
-import cheerio from 'cheerio'
+import { Node } from '../Node';
+import { NodeParameter } from '../../NodeParameter';
+import axios, { AxiosRequestConfig } from 'axios';
+import { Feature } from '../../Feature';
+import cheerio from 'cheerio';
 
 export class ScrapeHTML extends Node {
-	constructor(options = {}) {
-		super({
-			// Defaults
-			name: 'ScrapeHTML',
-			summary: 'Scrapes static HTML content lists. Might need a proxy.',
-			category: 'Workflow',
-			defaultInPorts: [],
-			defaultOutPorts: ['Output', 'Failed'],			
-			// Explicitly configured
-			...options,
-		})
-	}
+  constructor(options = {}) {
+    super({
+      // Defaults
+      name: 'ScrapeHTML',
+      summary:
+        'Scrapes static HTML content lists. Might need a proxy.',
+      category: 'Workflow',
+      defaultInPorts: [],
+      defaultOutPorts: ['Output', 'Failed'],
+      // Explicitly configured
+      ...options,
+    });
+  }
 
-	async run() {
-		const proxy = this.getParameterValue('proxy')
-		const url = this.getParameterValue('url')
-		const fullUrl = (proxy ? proxy + '/': '') + url
-		const selector = this.getParameterValue('Root selector')
-		const config = {
-			'X-Requested-With': 'XMLHttpRequest'
-		} as AxiosRequestConfig
-	
-		await axios.get(fullUrl, config)
-			.then((response) => {
-				const $ = cheerio.load(response.data);
-				
-				const elements = $(selector);
-				const features = this.toFeatures(elements)
-				this.output(features)
-			})
-			.catch((reason) => {
-				console.log(reason)
-				throw(reason)
-			});			
-	}
-	
-	toFeatures(elements): Feature[] {
-		const features = []
-		
-		for (let i = 0; i < elements.length; i++) {
-			const root = elements[i]
-			features.push(this.extractFeature(root))
-		}
+  async run() {
+    const proxy = this.getParameterValue('proxy');
+    const url = this.getParameterValue('url');
+    const fullUrl = (proxy ? proxy + '/' : '') + url;
+    const selector =
+      this.getParameterValue('Root selector');
+    const config = {
+      'X-Requested-With': 'XMLHttpRequest',
+    } as AxiosRequestConfig;
 
-		return features
-	}
+    await axios
+      .get(fullUrl, config)
+      .then((response) => {
+        const $ = cheerio.load(response.data);
 
-	extractFeature(element): Feature {
-    const extractions = this.getParameterValue('Attribute extractions');		
-		const feature = new Feature({})
+        const elements = $(selector);
+        const features = this.toFeatures(elements);
+        this.output(features);
+      })
+      .catch((reason) => {
+        console.log(reason);
+        throw reason;
+      });
+  }
 
-		extractions.forEach((extraction) => {
-			const attributeName = extraction['attribute']['value']
-			const selector = extraction['selector']['value']
-			const method = extraction['method']['value']
-			
-			feature.set(
-				attributeName,
-				this.extractAttributeValue(element, selector, method)
-			)
-		});
+  toFeatures(elements): Feature[] {
+    const features = [];
 
-		return feature
-	}
+    for (let i = 0; i < elements.length; i++) {
+      const root = elements[i];
+      features.push(this.extractFeature(root));
+    }
 
-	extractAttributeValue(element, selector, method = 'single'): string {
-		const $ = cheerio.load(element);
+    return features;
+  }
 
-		if(method != 'single') return;
+  extractFeature(element): Feature {
+    const extractions = this.getParameterValue(
+      'Attribute extractions',
+    );
+    const feature = new Feature({});
 
-		return $(selector)[0].firstChild.data
-	}
+    extractions.forEach((extraction) => {
+      const attributeName =
+        extraction['attribute']['value'];
+      const selector = extraction['selector']['value'];
+      const method = extraction['method']['value'];
+
+      feature.set(
+        attributeName,
+        this.extractAttributeValue(
+          element,
+          selector,
+          method,
+        ),
+      );
+    });
+
+    return feature;
+  }
+
+  extractAttributeValue(
+    element,
+    selector,
+    method = 'single',
+  ): string {
+    const $ = cheerio.load(element);
+
+    if (method != 'single') return;
+
+    return $(selector)[0].firstChild.data;
+  }
 
   getDefaultParameters() {
     return [
       ...super.getDefaultParameters(),
-			NodeParameter.string('url').withValue('https://www.hemnet.se/bostader'),
-			NodeParameter.string('proxy').withValue('https://nameless-dawn-49509.herokuapp.com'),
-			NodeParameter.string('Root selector').withValue('.normal-results__hit'),
+      NodeParameter.string('url').withValue(
+        'https://www.hemnet.se/bostader',
+      ),
+      NodeParameter.string('proxy').withValue(
+        'https://nameless-dawn-49509.herokuapp.com',
+      ),
+      NodeParameter.string('Root selector').withValue(
+        '.normal-results__hit',
+      ),
       NodeParameter.row('Attribute extractions', [
         NodeParameter.string('attribute').withValue(
           'address',
@@ -91,11 +111,11 @@ export class ScrapeHTML extends Node {
         NodeParameter.string('selector').withValue(
           '.listing-card__street-address',
         ),
-				// Refactor to select
+        // Refactor to select
         NodeParameter.string('method')
-					.withPlaceholder('single || multiple') // Ensure this works
-					.withValue('single') // Only single implemented at the moment
+          .withPlaceholder('single || multiple') // Ensure this works
+          .withValue('single'), // Only single implemented at the moment
       ]).repeatable(),
     ];
-  }	
+  }
 }
--- tests/Unit/server/nodes/ScrapeHTML.test.skip.ts
+++ tests/Unit/server/nodes/ScrapeHTML.test.skip.ts
@@ -1,11 +1,11 @@
-import { ScrapeHTML } from '../../../../src/server/nodes/ScrapeHTML'
-import { when } from "../NodeTester";
+import { ScrapeHTML } from '../../../../src/server/nodes/ScrapeHTML';
+import { when } from '../NodeTester';
 
 describe('ScrapeHTML node', () => {
-	it('DESCRIPTION', async () => {
-		
-		await when(ScrapeHTML).hasDefaultParameters()
-			.assertCanRun()
-			.finish()
-	});
+  it('DESCRIPTION', async () => {
+    await when(ScrapeHTML)
+      .hasDefaultParameters()
+      .assertCanRun()
+      .finish();
+  });
 });
