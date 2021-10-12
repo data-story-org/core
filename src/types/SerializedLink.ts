ab56a53768b17219d5b0343848880ef40855646a
Anders Jurisoo <jurisoo@hotmail.com>
2021-10-12 23:00:18 +0200

    Remove unused type

--- src/types/SerializedLink.ts
+++ src/types/SerializedLink.ts
@@ -3,24 +3,3 @@ export type SerializedLink = {
   sourcePort: string;
   targetPort: string;
 };
-
-// TO BE MOVED
-export type SerializedReactLink = {
-  id: string;
-  type: string;
-  source: string;
-  sourcePort: string;
-  target: string;
-  targetPort: string;
-  points: {
-    id: string;
-    type: string;
-    x: number;
-    y: number;
-  }[];
-  labels: [];
-  width: number;
-  color: string;
-  curvyness: number;
-  selectedColor: string;
-};
