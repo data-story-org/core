8f67e96307e522ed83d967fa1e4f190b32e12c00
ajthinking <ajthinking@users.noreply.github.com>
2021-10-26 17:05:22 +0000

    Apply formatting changes

--- tests/Unit/server/nodes/RunDiagram.test.ts
+++ tests/Unit/server/nodes/RunDiagram.test.ts
@@ -1,41 +1,45 @@
 import { DiagramBuilder } from '../../../../src/server/DiagramBuilder';
-import { Create, CreateJSON, CreateSequence, Input, Inspect, Output } from '../../../../src/server/nodes';
-import { RunDiagram } from '../../../../src/server/nodes/RunDiagram'
-import { when } from "../NodeTester";
+import {
+  Create,
+  CreateJSON,
+  CreateSequence,
+  Input,
+  Inspect,
+  Output,
+} from '../../../../src/server/nodes';
+import { RunDiagram } from '../../../../src/server/nodes/RunDiagram';
+import { when } from '../NodeTester';
 import { Feature } from '../../../../src/Feature';
-import { nonCircularJsonStringify } from "../../../../src/utils/nonCircularJsonStringify"
+import { nonCircularJsonStringify } from '../../../../src/utils/nonCircularJsonStringify';
 
 const dummyDiagramJson = () => {
-	const diagram = DiagramBuilder.begin()
-		.add(Input)
-		.add(Output)
-		.finish()
+  const diagram = DiagramBuilder.begin()
+    .add(Input)
+    .add(Output)
+    .finish();
 
-	return nonCircularJsonStringify(
-		diagram.serialize()
-	)
-}
+  return nonCircularJsonStringify(diagram.serialize());
+};
 
 describe('RunDiagram node', () => {
-	it('can run', async () => {
-		await when(RunDiagram)
-			.hasParameters({ diagram: dummyDiagramJson() })
-			.assertCanRun()
-			.finish()
-	});
+  it('can run', async () => {
+    await when(RunDiagram)
+      .hasParameters({ diagram: dummyDiagramJson() })
+      .assertCanRun()
+      .finish();
+  });
 
-	it('will pass on inputs to outputs', async () => {
-		const diagram = DiagramBuilder.begin()
-			.add(Create)
-			.add(RunDiagram, { diagram: dummyDiagramJson() })
-			.add(Output)
-			.finish()
+  it('will pass on inputs to outputs', async () => {
+    const diagram = DiagramBuilder.begin()
+      .add(Create)
+      .add(RunDiagram, { diagram: dummyDiagramJson() })
+      .add(Output)
+      .finish();
 
-		await diagram.run()
+    await diagram.run();
 
-		expect(diagram.getOutputFeatures())
-			.toStrictEqual([
-				new Feature({ resource: 'todos' })
-			])
-	});
+    expect(diagram.getOutputFeatures()).toStrictEqual([
+      new Feature({ resource: 'todos' }),
+    ]);
+  });
 });
